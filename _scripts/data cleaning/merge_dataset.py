12571df12273934d0541b6e91abb952976eeda4b
Bennet Hanke <hankeb@students.uni-marburg.de>
2025-05-22 13:43:01 +0200

    integrated missing_year_extraction into merge_dataset.py, filenames file is already updated

--- _scripts/data cleaning/merge_dataset.py
+++ _scripts/data cleaning/merge_dataset.py
@@ -5,6 +5,25 @@ import pandas as pd
 #%% LOAD DATASET WITH METADATA
 scriptum_df = pd.read_json("https://raw.githubusercontent.com/CCS-ZCU/scriptum/refs/heads/master/data/files_df.json")
 
+#%% ENRICH DATA WITH YEARS FROM FILENAMES
+mask = scriptum_df['year'].isna()
+for idx in scriptum_df[mask].index:
+    filename = scriptum_df.at[idx, 'filename']
+    if '88' in filename:
+        scriptum_df.at[idx, 'year'] = 1988.0
+    elif '89' in filename:
+        scriptum_df.at[idx, 'year'] = 1989.0
+    elif '87' in filename:
+        scriptum_df.at[idx, 'year'] = 1987.0
+    elif '51' in filename and scriptum_df.at[idx, 'filename'] == 'novy-brak_05_chybi-str-51_ocr.txt':
+        # see https://scriptum.cz/cs/periodika/novy-brak
+        scriptum_df.at[idx, 'year'] = 1983.0
+    elif '71' in filename:
+        # see https://scriptum.cz/cs/periodika/poradni-svitek
+       continue
+    else:
+        continue
+
 # CREATE NEW COLUMN
 sum(scriptum_df["year"].apply(lambda x: 1948 <= x <= 1989))
 scriptum_df["communism"] = scriptum_df["year"].apply(lambda x: 1948 <= x <= 1989)
@@ -26,4 +45,6 @@ filenames = scriptum_subset['filename'].to_list()
 
 with open("filenames_SCRIPTUM_1971-1999.txt", mode='w') as file:
     file.write('\n'.join(filenames))
+
+
 # %%
